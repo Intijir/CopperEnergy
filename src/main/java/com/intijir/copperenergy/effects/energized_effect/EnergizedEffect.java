80b13664574e7bdcb1f010d121ef689c31b75852
Intijir <hashemdroubi18@gmail.com>
2023-01-02 22:48:19 +0300

    assets

---
+++ src/main/java/com/intijir/copperenergy/effects/energized_effect/EnergizedEffect.java
@@ -0,0 +1,24 @@
+package com.intijir.copperenergy.effects.energized_effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+
+public class EnergizedEffect extends MobEffect {
+    public EnergizedEffect(MobEffectCategory mobEffectCategory, int color) {
+        super(mobEffectCategory, color);
+    }
+
+    @Override
+    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+
+        super.applyEffectTick(pLivingEntity, pAmplifier);
+    }
+
+    @Override
+    public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
+        return true;
+    }
+
+
+}
